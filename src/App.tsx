69603b84f698e0b006af2a0462bd86c33a115d3b
Andreas Lambropoulos <andlamb2002@gmail.com>
2025-07-25 13:48:38 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Timer from './Components/Timer';
+import '@picocss/pico';
+
+
+function App() {
+  return (
+    <>
+      <Timer />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Timer.tsx
@@ -0,0 +1,78 @@
+import { useState, useEffect, useRef } from 'react'
+
+function Timer() {
+
+    const [isRunning, setIsRunning] = useState<boolean>(false);
+    const [time, setTime] = useState<number>(0);
+    const intervalIdRef = useRef<number | undefined>(undefined);
+    const startTimeRef = useRef<number>(0);
+    const isRunningRef = useRef<boolean>(isRunning);
+    const hasStoppedRef = useRef<boolean>(false);
+    
+    function start() {
+        setIsRunning(true);
+        startTimeRef.current = Date.now() - time;
+    }
+
+    function stop() {
+        setIsRunning(false);
+        hasStoppedRef.current = true;
+    }
+
+    useEffect(() => {
+        if(isRunning) {
+            intervalIdRef.current = setInterval(() => {
+                setTime(Date.now() - startTimeRef.current);
+            }, 10)
+        }
+
+        return () => {
+            clearInterval(intervalIdRef.current);
+            intervalIdRef.current = undefined;
+        }
+
+    }, [isRunning]);
+
+    useEffect (() => {
+        isRunningRef.current = isRunning;
+    }, [isRunning]);
+
+    useEffect(() => {
+        const handleKeyDown = () => {
+            if (isRunningRef.current) {
+                stop();
+            }
+        }
+
+        const handleKeyUp = (event: KeyboardEvent) => {
+            if (event.code === 'Space' && !isRunningRef.current && !hasStoppedRef.current) {
+                start();
+            }
+
+            if (hasStoppedRef.current) {
+                hasStoppedRef.current = false;
+            }
+        }
+
+        document.addEventListener('keydown', handleKeyDown);
+        document.addEventListener('keyup', handleKeyUp);
+
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+            document.removeEventListener('keyup', handleKeyUp);
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    function formatTime(): string {
+        return (time / 1000).toFixed(2);
+    }
+
+    return (
+        <>
+            {formatTime()}
+        </>
+    );
+}
+
+export default Timer
