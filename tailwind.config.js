80b7302b94d50d02be249d03c6f9a1f9ac7d1c8e
Andreas Lambropoulos <andlamb2002@gmail.com>
2025-08-29 12:17:07 -0400

    Font setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+    darkMode: 'class', // or "media" if you want
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['"Open Sans"', 'sans-serif'],
+            },
+        },
+    },
+}
