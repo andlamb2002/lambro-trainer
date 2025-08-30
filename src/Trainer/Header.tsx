6884388e0497e11efb3483fae8e435fc0e378ddc
Andreas Lambropoulos <andlamb2002@gmail.com>
2025-08-30 01:58:03 -0400

    Updated links

--- src/Trainer/Header.tsx
+++ src/Trainer/Header.tsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
 
 function Header() {
-    // Add nav later
     return (
         <header className="bg-app-header-dark text-app-text-dark">
-                <Link to="/pll">PLL</Link> | <Link to="/oll">OLL</Link>
+            <nav>
+                <Link to="/pll" className="link">PLL</Link>
+                <Link to="/oll" className="link">OLL</Link>
+            </nav>
         </header>
     )
 }
