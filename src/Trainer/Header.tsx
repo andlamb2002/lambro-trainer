65169c1fb864d6dc7ed5d7a4e16fddf22b197570
Andreas Lambropoulos <andlamb2002@gmail.com>
2025-08-29 23:00:38 -0400

    Tailwind color and font setup

--- src/Trainer/Header.tsx
+++ src/Trainer/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 function Header() {
     // Add nav later
     return (
-        <header>
+        <header className="bg-app-header-dark text-app-text-dark">
                 <Link to="/pll">PLL</Link> | <Link to="/oll">OLL</Link>
         </header>
     )
