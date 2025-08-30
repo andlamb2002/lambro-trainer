0c8a3434caa02ad0ba71db9d553a1b34fb6167bb
Andreas Lambropoulos <andlamb2002@gmail.com>
2025-08-30 17:09:48 -0400

    Styled header

--- src/Trainer/Header.tsx
+++ src/Trainer/Header.tsx
@@ -7,17 +7,22 @@ interface Props {
 
 function Header({ darkMode, setDarkMode }: Props) {
     return (
-        <header className="bg-secondary">
-            <nav>
-                <Link to="/pll" className="link">PLL</Link>
-                <Link to="/oll" className="link">OLL</Link>
-            </nav>
-            <button
-                onClick={() => setDarkMode(!darkMode)}
-                className="btn btn-primary"
-            >
-                {darkMode ? "Light" : "Dark"}
-            </button>
+        <header className="flex items-center justify-between bg-secondary px-4 py-8 shadow-lg">
+            <div className="text-2xl font-bold">
+                Lambro Trainer
+            </div>
+            <div className="flex items-center gap-4 pr-4">
+                <nav className="flex gap-4">
+                    <Link to="/pll" className="link">PLL</Link>
+                    <Link to="/oll" className="link">OLL</Link>
+                </nav>
+                <button
+                    onClick={() => setDarkMode(!darkMode)}
+                    className="btn btn-primary"
+                >
+                    {darkMode ? "Light" : "Dark"}
+                </button>
+            </div>
         </header>
     )
 }
