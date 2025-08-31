2c926b0c8e66233ac968674ea1b882900ca941d5
Andreas Lambropoulos <andlamb2002@gmail.com>
2025-08-31 00:36:27 -0400

    Fix train button

--- src/Trainer/Components/CasesSection.tsx
+++ src/Trainer/Components/CasesSection.tsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import CaseItem from "./CaseItem";
 import type { Case } from "../interfaces";
 
+import { MdArrowForward } from "react-icons/md";
+
 interface Props {
     cases: Case[];
     toggleCase: (id: string) => void;
@@ -28,18 +30,21 @@ function CasesSection({ cases, toggleCase, toggleAllCases, toggleCasesInSet }: P
 
     return (
         <div>
-            <button
-                className="btn btn-primary"
-                onClick={() => navigate("timer")}
-            >
-                Start
-            </button>
-
-            <div className="flex gap-2 items-end">
-                <h2 className="text-xl font-bold underline">Case Selection</h2>
-                <button className="btn btn-success" onClick={() => toggleAllCases(true)}>All</button>
-                <button className="btn btn-danger" onClick={() => toggleAllCases(false)}>None</button>
+            <div className="flex justify-between">
+                <div className="flex gap-2 items-end">
+                    <h2 className="text-xl font-bold underline">Case Selection</h2>
+                    <button className="btn btn-success" onClick={() => toggleAllCases(true)}>All</button>
+                    <button className="btn btn-danger" onClick={() => toggleAllCases(false)}>None</button>
+                </div>
+                <button
+                    className="btn btn-primary flex items-center gap-2 text-2xl font-bold px-4 py-2"
+                    onClick={() => navigate("timer")}
+                >
+                    Train
+                    <MdArrowForward size={24} />
+                </button>
             </div>
+            
             
             {Object.entries(groupedCases).map(([setName, setCases]) => (
                 <div key={setName}>
