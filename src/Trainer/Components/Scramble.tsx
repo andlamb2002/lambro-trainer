da70f9ae2edb6e28756f38ef3f0a4f2ecb4523a4
Andreas Lambropoulos <andlamb2002@gmail.com>
2025-08-31 00:55:30 -0400

    Style of scramble in timer

--- src/Trainer/Components/Scramble.tsx
+++ src/Trainer/Components/Scramble.tsx
@@ -2,6 +2,8 @@ import { useNavigate } from 'react-router-dom';
 
 import type { Case } from '../interfaces';
 
+import { MdArrowBack } from "react-icons/md";
+
 interface Props {
     currentScramble: string;
     recapMode: boolean;
@@ -14,29 +16,32 @@ function Scramble({ currentScramble, recapMode, recapQueue, recapIndex, toggleRe
 
     const navigate = useNavigate();
 
+
     return (
-        <>
-            <button
-                className="btn btn-primary"
-                onClick={() => navigate("..")}
-            >
-                Select
-            </button>
-            <h3>
-                {currentScramble}
-            </h3>
-            <div>
-                <button className="btn btn-primary" onClick={toggleRecap}>
-                {recapMode ? 'End Recap' : 'Start Recap'}
+        <div className="flex justify-between items-center mb-4">
+            <div className="flex items-center gap-8">
+                <button
+                    className="btn btn-primary flex items-center gap-1 text-2xl font-bold px-4 py-2"
+                    onClick={() => navigate("..")}
+                >
+                    <MdArrowBack size={24} />
+                    Select
                 </button>
-                
-                {recapMode && (
-                <span>
-                    Recap Progress: {recapIndex + 1} / {recapQueue.length}
-                </span>
-                )}
+                <h3 className="font-bold text-2xl">
+                    {currentScramble}
+                </h3>
             </div>
-        </>
+            <div className="flex items-center gap-2">
+                {recapMode && (
+                    <div>
+                        Progress: {recapIndex + 1} / {recapQueue.length}
+                    </div>
+                )}
+                <button className="btn btn-primary text-2xl font-bold px-4 py-2" onClick={toggleRecap}>
+                    {recapMode ? 'End' : 'Recap'}
+                </button>
+            </div>
+        </div>
     )
 }
 
