217ffd19bea91755c8bb9f4b80594ac65e4e6910
Andreas Lambropoulos <andlamb2002@gmail.com>
2025-09-01 20:40:10 -0400

    Custom domain setup

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/lambro-trainer">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
