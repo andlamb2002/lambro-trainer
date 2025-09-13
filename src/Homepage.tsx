f2274bcaed8f530b33ec375d5c4cc725e1624313
Andreas Lambropoulos <andlamb2002@gmail.com>
2025-09-13 01:09:47 -0400

    Added labels/titles as tooltips

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 function Homepage() {
     return (
-        <div className="grid grid-cols-2 sm:w-3/5 mt-4 gap-2 sm:gap-8 mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-3 w-3/5 mt-4 gap-2 sm:gap-8 mx-auto">
             
-            <Link to="/pll" className="link flex flex-col items-center rounded hover:bg-secondary">
+            <Link to="/pll" className="link flex flex-col items-center rounded hover:bg-secondary p-2" title="PLL Trainer" aria-label="PLL Trainer">
                 <img
                     src="https://visualcube.api.cubing.net/visualcube.php?fmt=svg&view=plan&bg=t&case=RUR'U'R'FR2U'R'U'RUR'F'"
                     alt="PLL Trainer"
@@ -13,7 +13,7 @@ function Homepage() {
                 <span className="mt-2 text-2xl font-bold">PLL Trainer</span>
             </Link>
 
-            <Link to="/oll" className="link flex flex-col items-center rounded hover:bg-secondary">
+            <Link to="/oll" className="link flex flex-col items-center rounded hover:bg-secondary p-2" title="OLL Trainer" aria-label="Oll Trainer">
                 <img
                     src="https://visualcube.api.cubing.net/visualcube.php?fmt=svg&view=plan&stage=oll&bg=t&case=RUR'U'R'FRF'"
                     alt="OLL Trainer"
@@ -22,7 +22,7 @@ function Homepage() {
                 <span className="mt-2 text-2xl font-bold">OLL Trainer</span>
             </Link>
 
-            <Link to="/ollcp" className="link flex flex-col items-center rounded hover:bg-secondary">
+            <Link to="/ollcp" className="link flex flex-col items-center rounded hover:bg-secondary p-2" title="OLLCP Trainer" aria-label="OLLCP Trainer">
                 <img
                     src="https://visualcube.api.cubing.net/visualcube.php?fmt=svg&view=plan&stage=coll&bg=t&case=D2U2B2L'B'LR2FD2F'U'F2B2L2U2B2D'B2DF2"
                     alt="OLLCP Trainer"
