a2b9012c245f4c65c61b5f0a08c195446c775467
Andreas Lambropoulos <andlamb2002@gmail.com>
2025-09-15 00:19:36 -0400

    Moved recap details to left, removed duplicated code

--- src/Trainer/Components/Scramble.tsx
+++ src/Trainer/Components/Scramble.tsx
@@ -15,8 +15,8 @@ function Scramble({ currentScramble, recapMode, recapProgress, recapTotal, toggl
     const navigate = useNavigate();
 
     return (
-        <div className="flex flex-col sm:flex-row sm:justify-between items-center gap-2 sm:gap-0 sm:mb-4 sm:px-4">
-            <div className="flex w-full justify-between sm:w-auto sm:justify-normal sm:items-center gap-2 sm:gap-8">
+        <div className="flex flex-col sm:flex-row sm:items-center gap-2 sm:mb-4 sm:px-4">
+            <div className="flex sm:items-center gap-2">
                 <button
                     className="btn btn-primary flex items-center gap-1 text-2xl font-bold w-auto self-start sm:self-auto px-2 py-1 sm:px-4 sm:py-2"
                     onClick={() => navigate("..")}
@@ -26,15 +26,7 @@ function Scramble({ currentScramble, recapMode, recapProgress, recapTotal, toggl
                     <MdArrowBack size={24} />
                     Select
                 </button>
-                <h3 className="hidden sm:block font-bold text-2xl">
-                    {currentScramble}
-                </h3>
-                <div className="sm:hidden flex items-center whitespace-nowrap gap-2">
-                    {recapMode && (
-                        <div>
-                            {recapProgress} / {recapTotal}
-                        </div>
-                    )}
+                <div className="flex items-center whitespace-nowrap gap-2">
                     <button 
                         className="btn btn-primary text-2xl font-bold px-2 py-1 sm:px-4 sm:py-2" 
                         onClick={toggleRecap}
@@ -43,24 +35,14 @@ function Scramble({ currentScramble, recapMode, recapProgress, recapTotal, toggl
                     >
                         {recapMode ? 'End' : 'Recap'}
                     </button>
+                    {recapMode && (
+                        <div>
+                            {recapProgress} / {recapTotal}
+                        </div>
+                    )}
                 </div>
             </div>
-            <div className="hidden sm:flex items-center whitespace-nowrap gap-2">
-                {recapMode && (
-                    <div>
-                        {recapProgress} / {recapTotal}
-                    </div>
-                )}
-                <button 
-                    className="btn btn-primary text-2xl font-bold px-2 py-1 sm:px-4 sm:py-2" 
-                    onClick={toggleRecap}
-                    title={recapMode ? "End Recap" : "Recap Each Selected Case"}
-                    aria-label={recapMode ? "End Recap" : "Recap Each Selected Case"}
-                >
-                    {recapMode ? 'End' : 'Recap'}
-                </button>
-            </div>
-            <h3 className="sm:hidden font-bold text-2xl select-text">
+            <h3 className="font-bold text-2xl sm:pl-8">
                 {currentScramble}
             </h3>
         </div>
